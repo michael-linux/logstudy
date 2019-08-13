a945a9c8da56581e361c5a26654c10488ee26415
michael-linux <michael_linux@163.com>
2019-08-13 23:17:18 +0800

    init

---
+++ src/main/java/com/michael/log/commonslogging/Test01.java
@@ -0,0 +1,12 @@
+package com.michael.log.commonslogging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Log log = LogFactory.getLog(Test01.class);
+		log.debug("dddddddddddddddddd");
+	}
+
+}
---
+++ src/main/java/com/michael/log/slf4j/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.michael.log.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    logger.info("Hello World");
+  }
+}
